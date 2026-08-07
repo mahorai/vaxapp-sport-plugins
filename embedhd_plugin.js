1aa641c3ecf84c1ee66d271483ac132b143ecf39
nicolainani <307027773+nicolainani@users.noreply.github.com>
2026-08-07 22:17:04 +0700

    chore: add crackstreams, methstreams plugin and logic code embed plugin

--- embedhd_plugin.js
+++ embedhd_plugin.js
@@ -6,7 +6,7 @@ function getManifest() {
   return JSON.stringify({
     id: "embedhd",
     name: "EmbedHD",
-    version: "1.0.4",
+    version: "1.0.5",
     baseUrl: "https://embedhd.st",
     iconUrl: "https://i.ibb.co/wrrMVcwk/embedhd-logo.jpg",
     isEnabled: true,
@@ -90,10 +90,7 @@ function parseListResponse(html, apiUrl) {
       const cardsHtml = extractCardsHtml(html, pattern, "div");
       // console.log(cardsHtml)
       // extract item
-      cardsHtml.forEach((cardHtml) => {
-        const event = extractItem(cardHtml);
-        if (event) items.push(event);
-      });
+      cardsHtml.forEach((cardHtml) => { extractItem(cardHtml) });
     }
     let streams = null;
     const items = [];
@@ -112,7 +109,7 @@ function parseListResponse(html, apiUrl) {
           title: stream.dataTitle,
           posterUrl: stream.leagueLogo,
           backdropUrl: stream.leagueLogo,
-          description: `Event "${stream.title}" is hosted on server EmbedHD`,
+          description: `Event "${stream.dataTitle}" is hosted on server EmbedHD`,
           quality: tLInfo,
           episode_current: "HD",
           lang: `${stream.dataCat} - ${stream.leagueTitle}`.toUpperCase()
